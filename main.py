2be08624cda5fe4775df70c90ca366583d33ea1f
Konstantin Kulikin <konstantin2000@gmail.com>
2019-12-26 23:40:33 +0200

    1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Исполльзавал информацию с:
+# https://younglinux.info/tkinter/canvasmeth.php
 import tkinter
 
 # import random
@@ -7,41 +9,73 @@ SIZE_OF_ROOT_WINDOWS = '800x600'
 WIDTH_OF_CANVAS = 800
 HEIGHT_OF_CANVAS = 600
 COLOR_OF_CANVAS = 'green'
-DELAY = 80
+DELAY = 40
+R = 20
 
 
-def update_of_canvas():
-    pppp = canvas.create_oval(140, 140, 160, 160, outline='red', fill='red')
-    canvas.bind('<Up>', lambda event: canvas.move(pppp, 0, -2))
-    canvas.bind('<Down>', lambda event: canvas.move(pppp, 0, 2))
-    canvas.bind('<Left>', lambda event: canvas.move(pppp, -2, 0))
-    canvas.bind('<Right>', lambda event: canvas.move(pppp, 2, 0))
+def create_world():
+    """Эта функция создает полотно,
+    а на нем шар который перемещается стрелками клавиатуры.
+    Клавиша «p» распечатывает текущие координаты
+    левого верхнего и правого нижнего угла описывающего круг квадрата.
+    Клавиша «Home» возвращает шар в левый верхний угол
+    Клавиша Delete удаляет шар
 
+    Второй шар перемещается, пока не достигнет правой стороны поля
+    """
+    canvas = tkinter.Canvas(root_window, width=WIDTH_OF_CANVAS, height=HEIGHT_OF_CANVAS, bg=COLOR_OF_CANVAS)
+    canvas.pack()
+    ball = canvas.create_oval(0, 0, R, R, outline='red', fill='red')
+    canvas.focus_set()
+    canvas.bind('<Up>', lambda event: canvas.move(ball, 0, -2))
+    canvas.bind('<Down>', lambda event: canvas.move(ball, 0, 2))
+    canvas.bind('<Left>', lambda event: canvas.move(ball, -2, 0))
+    canvas.bind('<Right>', lambda event: canvas.move(ball, 2, 0))
+    canvas.bind('p', lambda event: print(canvas.coords(ball)))
+    canvas.bind('<Delete>', lambda event: canvas.delete(ball))
+    canvas.bind()
 
-def update_of_ball():
-    global x0, y0, vx, vy, t
-    t = t + DELAY/100
-    x = x0 + vx * t
-    y = y0 + vy * t + 0.2 * t**2
-    print(t, x, y)
-    canvas.coords(ball, x, y, x + 40, y + 40)
-    root_of_window.after(DELAY, update_of_ball)
+    def to_home(event):
+        canvas.coords(ball, 0, 0, R, R)
+        print("Возвращаю шар в «Домой»")
+
+    canvas.bind('<Home>', to_home)
+
+    ball2 = canvas.create_oval(40, 40, 40 + R, 40 + R, outline='blue', fill='blue')
+
+    def mouse_click(event):
+        print("Клик по координатам:", event.x, event.y)
+
+    canvas.bind('<Button-1>', mouse_click)
+
+    def del_ball2(event):
+        print("Попал по ball2")
+        canvas.delete(ball2)
+
+    canvas.tag_bind(ball2, '<Button-1>', del_ball2)
+
+    def motion():
+        """
+        Бесконечный цикл блокирует обновление графического интерфейса (если он выполняется не в отдельном потоке).
+        Вместо него лучше использовать метод after.
+        Другой вариант - в цикле явно вызывать root_window.update() для обновления интерфейса.
+        """
+        try:
+            # print("Текущие координаты ball2:", canvas.coords(ball2)[2], canvas.coords(ball2)[3])
+            canvas.move(ball2, 2, 1)
+            if canvas.coords(ball2)[2] < WIDTH_OF_CANVAS:
+                root_window.after(DELAY, motion)
+        except:
+            pass
+
+    motion()
 
 
 # create a window, the canvas and start game
-root_of_window = tkinter.Tk()
-root_of_window.title("Полёт шаров")
-root_of_window.geometry(SIZE_OF_ROOT_WINDOWS)
-canvas = tkinter.Canvas(root_of_window, width=WIDTH_OF_CANVAS, height=HEIGHT_OF_CANVAS, bg=COLOR_OF_CANVAS)
-print(type(canvas))
-canvas.pack()
-t = 0
-x0 = 0
-y0 = 0
-vx = 20
-vy = 0
-
-ball = canvas.create_oval(x0, y0, x0 + 40, y0 + 40, outline='red', fill='red')
-
-update_of_ball()
-root_of_window.mainloop()
+root_window = tkinter.Tk()
+root_window.title("Полёт шаров")
+root_window.geometry(SIZE_OF_ROOT_WINDOWS)
+b = tkinter.Button(root_window, text="Кнопка")
+b.pack()
+create_world()
+root_window.mainloop()
