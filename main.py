736c4b9404445065804302cd842d23aed20c8b07
Konstantin Kulikin <konstantin2000@gmail.com>
2019-12-30 19:44:54 +0200

    dgfdfgfg

--- main.py
+++ main.py
@@ -11,74 +11,60 @@ HEIGHT_OF_CANVAS = 600
 COLOR_OF_CANVAS = 'green'
 DELAY = 40
 R = 20
+VX = 2
+VY = 1
 
 
-def create_world():
-    """Эта функция создает полотно,
-    а на нем шар который перемещается стрелками клавиатуры.
-    Клавиша «p» распечатывает текущие координаты
-    левого верхнего и правого нижнего угла описывающего круг квадрата.
-    Клавиша «Home» возвращает шар в левый верхний угол
-    Клавиша Delete удаляет шар
+def to_home(event):
+    canvas.coords(ball, 0, 0, R, R)
+    print("Возвращаю шар в «Домой»")
 
-    Второй шар перемещается, пока не достигнет правой стороны поля
+
+def del_ball2(event):
+    print("Попал по ball2:", event.x, event.y, canvas.coords(ball2))
+    canvas.delete(ball2)
+
+
+def motion():  # Сюда помещать события совершаемые по времени
     """
-    canvas = tkinter.Canvas(root_window, width=WIDTH_OF_CANVAS, height=HEIGHT_OF_CANVAS, bg=COLOR_OF_CANVAS)
-    canvas.pack()
-    ball = canvas.create_oval(0, 0, R, R, outline='red', fill='red')
-    canvas.focus_set()
-    canvas.bind('<Up>', lambda event: canvas.move(ball, 0, -2))
-    canvas.bind('<Down>', lambda event: canvas.move(ball, 0, 2))
-    canvas.bind('<Left>', lambda event: canvas.move(ball, -2, 0))
-    canvas.bind('<Right>', lambda event: canvas.move(ball, 2, 0))
-    canvas.bind('p', lambda event: print(canvas.coords(ball)))
-    canvas.bind('<Delete>', lambda event: canvas.delete(ball))
-    canvas.bind()
-
-    def to_home(event):
-        canvas.coords(ball, 0, 0, R, R)
-        print("Возвращаю шар в «Домой»")
-
-    canvas.bind('<Home>', to_home)
-
-    ball2 = canvas.create_oval(40, 40, 40 + R, 40 + R, outline='blue', fill='blue')
-
-    def mouse_click(event):
-        print("Клик по координатам:", event.x, event.y)
-
-    canvas.bind('<Button-1>', mouse_click)
-
-    def del_ball2(event):
-        print("Попал по ball2:", event.x, event.y, canvas.coords(ball2))
-        canvas.delete(ball2)
-
-    canvas.tag_bind(ball2, '<Button-1>', del_ball2)
-
-    def motion():  # Сюда помещать события совершаемые по времени
-        """
-        Бесконечный цикл блокирует обновление графического интерфейса (если он выполняется не в отдельном потоке).
-        Вместо него лучше использовать метод after.
-        Другой вариант - в цикле явно вызывать root_window.update() для обновления интерфейса.
-        """
-        try:
-            # print("Текущие координаты ball2:", canvas.coords(ball2)[2], canvas.coords(ball2)[3])
-            canvas.move(ball2, 2, 1)
-            if canvas.coords(ball2)[2] < WIDTH_OF_CANVAS:
-                root_window.after(DELAY, motion)
-            else:
-                print("Достиг края")
-        except:
-            pass
-
-    motion()
+    Бесконечный цикл блокирует обновление графического интерфейса (если он выполняется не в отдельном потоке).
+    Вместо него лучше использовать метод after.
+    Другой вариант - в цикле явно вызывать root_window.update() для обновления интерфейса.
+    """
+    # print("Текущие координаты ball2:", canvas.coords(ball2)[2], canvas.coords(ball2)[3])
+    canvas.move(ball2, VX, VY)
+    root_window.after(DELAY, motion)
 
 
-# create a window, the canvas and start game
+"""Эта программа создает полотно,
+а на нем первый шар, который перемещается стрелками клавиатуры.
+Клавиша «p» распечатывает текущие координаты
+левого верхнего и правого нижнего угла описывающего круг квадрата.
+Клавиша «Home» возвращает шар в левый верхний угол
+Клавиша Delete удаляет шар
+
+Второй шар перемещается сам 
+"""
 root_window = tkinter.Tk()
 root_window.title("Полёт шаров")
 root_window.geometry(SIZE_OF_ROOT_WINDOWS)
 b = tkinter.Button(root_window, text="Кнопка")
 b.pack()
-create_world()
+canvas = tkinter.Canvas(root_window, width=WIDTH_OF_CANVAS, height=HEIGHT_OF_CANVAS, bg=COLOR_OF_CANVAS)
+canvas.pack()
+ball = canvas.create_oval(0, 0, R, R, outline='red', fill='red')
+ball2 = canvas.create_oval(40, 40, 40 + R, 40 + R, outline='blue', fill='blue', tag='ball2')
+
+canvas.focus_set()
+canvas.bind('<Up>', lambda event: canvas.move(ball, 0, -2))
+canvas.bind('<Down>', lambda event: canvas.move(ball, 0, 2))
+canvas.bind('<Left>', lambda event: canvas.move(ball, -2, 0))
+canvas.bind('<Right>', lambda event: canvas.move(ball, 2, 0))
+canvas.bind('<Delete>', lambda event: canvas.delete(ball))
+
+canvas.bind('p', lambda event: print(canvas.coords(ball2)))
+canvas.tag_bind('ball2', '<Button-1>', del_ball2)
+canvas.bind('<Home>', to_home)
+motion()
 root_window.mainloop()  # Метод mainloop() объекта Tk запускает главный цикл обработки событий,
 # что в том числе приводит к отображению главного окна со всеми его причиндалами на экране
