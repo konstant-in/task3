6a169f7f3085f78a01b3c33ca73954a663de6787
Konstantin Kulikin <konstantin2000@gmail.com>
2019-12-31 14:05:08 +0200

    gun.py - игра подправлена

--- gun.py
+++ gun.py
@@ -9,21 +9,27 @@ COLOR_OF_CANVAS = 'green'
 DELAY = 40
 R = 20
 R2 = 60
-VX1 = 10
-VY1 = 0
-VX2 = 0
-VY2 = -1
 
-g_fire = False
+
+vx1 = 0
+vy1 = 0
+vx2 = 0
+vy2 = 0
+ball_of_gun = None
+target = None
 
 def to_fire():
-    global g_fire
-    g_fire = True
+    global vx1, vy1
+    vx1 = 10
+    vy1 = 0
 
 def motion():
-    canvas.move(target, VX2, VY2)
-    if g_fire:
-        canvas.move(ball_of_gun, VX1, VY1)
+    if len(canvas.coords(ball_of_gun)) == 0:
+        ball_init()
+    if len(canvas.coords(target)) == 0:
+        tartet_init()
+    canvas.move(target, vx2, vy2)
+    canvas.move(ball_of_gun, vx1, vy1)
     if len(canvas.coords(ball_of_gun)) > 0 and target in canvas.find_overlapping(*canvas.coords(ball_of_gun)):
         print("Попал!!!")
         canvas.delete(target)
@@ -34,20 +40,23 @@ def motion():
     if len(canvas.coords(target)) > 0 and canvas.coords(target)[3] < 0:
         print('Цель улетела!')
         canvas.delete(target)
-    if len(canvas.coords(ball_of_gun)) == len(canvas.coords(target)) == 0:
-        print('Нет ни ядер, ни целей')
-        game_init()
     root_window.after(DELAY, motion)
 
-ball_of_gun = None
-target = None
 
-def game_init():
-    global ball_of_gun, target, g_fire
-    g_fire = False
-    print('Начинаем новую игру')
+def ball_init():
+    global ball_of_gun, vx1, vy1
+    vx1 = 0
+    vy1 = 0
+    print('Новое ядро')
     ball_of_gun = canvas.create_oval(0, HEIGHT_OF_CANVAS / 2, R, HEIGHT_OF_CANVAS / 2 + R, outline='red', fill='red')
+
+def tartet_init():
+    global target, vx2, vy2
+    vx2 = 0
+    vy2 = -1
+    print('Новая цель')
     target = canvas.create_oval(WIDTH_OF_CANVAS - R2, HEIGHT_OF_CANVAS, WIDTH_OF_CANVAS, HEIGHT_OF_CANVAS - R2, outline='blue', fill='blue', tag='ball2')
+
 
 root_window = tkinter.Tk()
 root_window.title("Пушка стреляющяя ядрами")
@@ -60,7 +69,8 @@ canvas.bind('<Down>', lambda event: canvas.move(ball_of_gun, 0, 10))
 canvas.bind('<Home>', lambda event: canvas.coords(ball_of_gun, 0, HEIGHT_OF_CANVAS / 2, R, HEIGHT_OF_CANVAS / 2 + R))
 canvas.bind('<space>', lambda event: to_fire())
 
-game_init()
+ball_init()
+tartet_init()
 
 motion()
 root_window.mainloop()
