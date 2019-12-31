68f0364464ddb8f9344ba73d63b8feb90ee0897c
Konstantin Kulikin <konstantin2000@gmail.com>
2019-12-31 14:41:54 +0200

    gun.py - игра подправлена

--- gun.py
+++ gun.py
@@ -10,50 +10,58 @@ DELAY = 40
 R = 20
 R2 = 60
 
-
 vx1 = 0
 vy1 = 0
 vx2 = 0
 vy2 = 0
+g = 0
 ball_of_gun = None
 target = None
 
 def to_fire():
-    global vx1, vy1
+    global vx1, vy1, g
     vx1 = 10
     vy1 = 0
+    g = .04
+
+def exsist_list(b):
+    return len(canvas.coords(b)) != 0
 
 def motion():
-    if len(canvas.coords(ball_of_gun)) == 0:
+    global vx1, vy1 ,vx2, vy2, g
+    if not exsist_list(ball_of_gun):
         ball_init()
     if len(canvas.coords(target)) == 0:
         tartet_init()
     canvas.move(target, vx2, vy2)
     canvas.move(ball_of_gun, vx1, vy1)
-    if len(canvas.coords(ball_of_gun)) > 0 and target in canvas.find_overlapping(*canvas.coords(ball_of_gun)):
+    vy1 += g
+    if exsist_list(ball_of_gun) and target in canvas.find_overlapping(*canvas.coords(ball_of_gun)):
         print("Попал!!!")
         canvas.delete(target)
         canvas.delete(ball_of_gun)
-    if len(canvas.coords(ball_of_gun)) > 0 and canvas.coords(ball_of_gun)[2] > WIDTH_OF_CANVAS:
+    if exsist_list(ball_of_gun) and canvas.coords(ball_of_gun)[2] > WIDTH_OF_CANVAS:
         print('Ядро улетело!')
         canvas.delete(ball_of_gun)
-    if len(canvas.coords(target)) > 0 and canvas.coords(target)[3] < 0:
+    if exsist_list(target) and canvas.coords(target)[3] < 0:
         print('Цель улетела!')
         canvas.delete(target)
     root_window.after(DELAY, motion)
 
 
 def ball_init():
-    global ball_of_gun, vx1, vy1
+    global ball_of_gun, vx1, vy1, g
     vx1 = 0
     vy1 = 0
+    g = 0
     print('Новое ядро')
     ball_of_gun = canvas.create_oval(0, HEIGHT_OF_CANVAS / 2, R, HEIGHT_OF_CANVAS / 2 + R, outline='red', fill='red')
 
 def tartet_init():
-    global target, vx2, vy2
+    global target, vx2, vy2, g
     vx2 = 0
     vy2 = -1
+    g = 0
     print('Новая цель')
     target = canvas.create_oval(WIDTH_OF_CANVAS - R2, HEIGHT_OF_CANVAS, WIDTH_OF_CANVAS, HEIGHT_OF_CANVAS - R2, outline='blue', fill='blue', tag='ball2')
 
