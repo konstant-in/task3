491e92f1b7d3592fbb8273c78e5b68ccca3c5705
Konstantin Kulikin <konstantin2000@gmail.com>
2019-12-31 12:41:13 +0200

    gun.py - игра подправлена

--- gun.py
+++ gun.py
@@ -3,8 +3,8 @@ import random
 
 # constants
 SIZE_OF_ROOT_WINDOWS = '800x600'
-WIDTH_OF_CANVAS = 800
-HEIGHT_OF_CANVAS = 600
+WIDTH_OF_CANVAS = 400
+HEIGHT_OF_CANVAS = 300
 COLOR_OF_CANVAS = 'green'
 DELAY = 40
 R = 20
@@ -16,25 +16,24 @@ VY2 = -1
 
 g_fire = False
 
-
 def to_fire():
     global g_fire
     g_fire = True
 
-
-def del_ball2(event):
-    print("Попал!!!")
-    canvas.delete(target)
-
-
 def motion():
+    canvas.move(target, VX2, VY2)
     if g_fire:
         canvas.move(ball_of_gun, VX1, VY1)
-    if target in canvas.find_overlapping(*canvas.coords(ball_of_gun)):
+    if len(canvas.coords(ball_of_gun)) > 0 and target in canvas.find_overlapping(*canvas.coords(ball_of_gun)):
         print("Попал!!!")
         canvas.delete(target)
-
-    canvas.move(target, VX2, VY2)
+        canvas.delete(ball_of_gun)
+    if len(canvas.coords(ball_of_gun)) > 0 and canvas.coords(ball_of_gun)[2] > WIDTH_OF_CANVAS:
+        print('Ядро улетело!')
+        canvas.delete(ball_of_gun)
+    if len(canvas.coords(target)) > 0 and canvas.coords(target)[3] < 0:
+        print('Цель улетела!')
+        canvas.delete(target)
     root_window.after(DELAY, motion)
 
 
@@ -45,14 +44,10 @@ canvas = tkinter.Canvas(root_window, width=WIDTH_OF_CANVAS, height=HEIGHT_OF_CAN
 canvas.pack()
 ball_of_gun = canvas.create_oval(0, HEIGHT_OF_CANVAS / 2, R, HEIGHT_OF_CANVAS / 2 + R, outline='red', fill='red')
 target = canvas.create_oval(WIDTH_OF_CANVAS - R2, HEIGHT_OF_CANVAS, WIDTH_OF_CANVAS, HEIGHT_OF_CANVAS - R2, outline='blue', fill='blue', tag='ball2')
-
 canvas.focus_set()
 canvas.bind('<Up>', lambda event: canvas.move(ball_of_gun, 0, -10))
 canvas.bind('<Down>', lambda event: canvas.move(ball_of_gun, 0, 10))
 canvas.bind('<Home>', lambda event: canvas.coords(ball_of_gun, 0, HEIGHT_OF_CANVAS / 2, R, HEIGHT_OF_CANVAS / 2 + R))
 canvas.bind('<space>', lambda event: to_fire())
-
-canvas.tag_bind('ball2', '<Button-1>', del_ball2)
-
 motion()
 root_window.mainloop()
