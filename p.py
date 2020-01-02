c13cc71577325c12aff4a65f67a215abc8a6db2f
Konstantin Kulikin <konstantin2000@gmail.com>
2020-01-02 10:59:45 +0200

    Изменен p.py

--- p.py
+++ p.py
@@ -11,18 +11,37 @@ DELAY = 80
 
 
 class Ball():
-    def __init__(self, r, x, y, speed_x, speed_y, c):
-        self.r = r
+    def __init__(self, x, y, radius, canvas):
         self.x = x
         self.y = y
-        self.speed_x = speed_x
-        self.speed_y = speed_y
-        self.c = c
+        self.radius = radius
+        self.canvas = canvas
+        self.wiev_ball = self.canvas.create_oval(self.x,
+                                                 self.y,
+                                                 self.x + self.radius,
+                                                 self.y + self.radius,
+                                                 outline='red',
+                                                 fill='red')
 
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+        self.canvas.move(self.wiev_ball, dx, dy)
 
 
 def keypress(event):
     print(event.keycode, event)
+
+
+def motion():
+    b.move(5, 3)
+    print(b.x, b.y)
+    root.after(DELAY, motion)
+
+
+def move_ball(ball, dx, dy):
+    canvas.move(ball.wiev_ball, dx, dy)
+
 
 # create a window, the canvas and start game
 root = tkinter.Tk()
@@ -30,9 +49,10 @@ root.title("Полёт шаров")
 root.geometry(SIZE_OF_ROOT_WINDOWS)
 canvas = tkinter.Canvas(root, width=WIDTH_OF_CANVAS, height=HEIGHT_OF_CANVAS, bg=COLOR_OF_CANVAS)
 canvas.pack()
-ball = Ball(20, 0, 0, 10, 20, canvas)
-ball.c.create_oval(ball.x, ball.y, ball.x + ball.r, ball.y + ball.r, outline='red', fill='red')
 
-root.bind('<Key>',keypress )
+b = Ball(0, 0, 20, canvas)
+
+root.bind('<Key>', keypress)
+motion()
 
 root.mainloop()
