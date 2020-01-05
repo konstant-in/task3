ca242ffde05a60bbec62238ba3e81bb667c837a3
Konstantin Kulikin <konstantin2000@gmail.com>
2020-01-05 12:47:27 +0200

    Изменен p.py

--- p.py
+++ p.py
@@ -6,25 +6,7 @@ HEIGHT_OF_CANVAS = 600
 COLOR_OF_CANVAS = 'green'
 DELAY = 80
 
-
-class Ball():
-    def __init__(self, x, y, radius, c):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.canvas = c
-        self.wiev_ball = self.canvas.create_oval(self.x,
-                                                 self.y,
-                                                 self.x + self.radius,
-                                                 self.y + self.radius,
-                                                 outline='red',
-                                                 fill='red')
-
-    def move(self, dx, dy):
-        self.x += dx
-        self.y += dy
-        self.canvas.move(self.wiev_ball, dx, dy)
-        # print(self.x, self.y)
+# шар полностью задается парой: canvas, id
 
 
 def keypress(event):
@@ -32,8 +14,16 @@ def keypress(event):
 
 
 def motion():
-    b.move(5, 3)
+    move_ball(canvas, ball_id, 5, 3)
     root.after(DELAY, motion)
+
+
+def create_ball(x, y, r, c):
+    return c.create_oval(x, y, x + r, y + r, outline='red', fill='red')
+
+
+def move_ball(c, b, dx, dy):
+    c.move(b, dx, dy)
 
 
 root = tkinter.Tk()
@@ -42,7 +32,7 @@ root.geometry(SIZE_OF_ROOT_WINDOWS)
 canvas = tkinter.Canvas(root, width=WIDTH_OF_CANVAS, height=HEIGHT_OF_CANVAS, bg=COLOR_OF_CANVAS)
 canvas.pack()
 
-b = Ball(0, 0, 20, canvas)
+ball_id = create_ball(0, 0, 20, canvas)
 
 root.bind('<Key>', keypress)
 motion()
