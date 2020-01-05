d360f7cf10c07cec170e3d3e048aba725622e139
Konstantin Kulikin <konstantin2000@gmail.com>
2020-01-05 14:11:46 +0200

    Изменен gun.py

--- gun.py
+++ gun.py
@@ -7,57 +7,67 @@ WIDTH_OF_CANVAS = 800
 HEIGHT_OF_CANVAS = 600
 COLOR_OF_CANVAS = 'green'
 DELAY = 40
-R = 20
+
+# радиусы ядра и цели
+R1 = 20
 R2 = 60
 
-vx1 = 0
-vy1 = 0
-vx2 = 0
-vy2 = 0
+# начальные координаты ядра и цели
+X1 = 0
+Y1 = HEIGHT_OF_CANVAS / 2
+X2 = WIDTH_OF_CANVAS - R2
+Y2 = HEIGHT_OF_CANVAS
+# скорости ядра и цели
+VX1 = 10
+VY1 = 0
+VX2 = 0
+VY2 = -1
+
+FIRE = False
 
 
 def to_fire():
-    global vx1, vy1
-    vx1 = 10
-    vy1 = 0
+    global FIRE
+    FIRE = True
 
-def exsist_list(b):
+
+def exsist_ball(b):
+    # шар существует, если длина списка его координат больше нуля
     return len(canvas.coords(b)) != 0
 
+
 def motion():
-    global ball_of_gun, target, vx1, vy1 ,vx2, vy2
-    if not exsist_list(ball_of_gun):
-        ball_of_gun = create_ball()
-    if not exsist_list(target):
-        target = create_target()
-    canvas.move(target, vx2, vy2)
-    canvas.move(ball_of_gun, vx1, vy1)
-    if exsist_list(ball_of_gun) and target in canvas.find_overlapping(*canvas.coords(ball_of_gun)):
+    global ball_of_gun, target
+    if not exsist_ball(ball_of_gun):
+        ball_of_gun = create_ball(X1, Y1, R1)
+    if not exsist_ball(target):
+        target = create_target(X2, Y2, R2)
+    if FIRE:
+        canvas.move(ball_of_gun, VX1, VY1)
+    canvas.move(target, VX2, VY2)
+    if exsist_ball(ball_of_gun) and target in canvas.find_overlapping(*canvas.coords(ball_of_gun)):
         print("Попал!!!")
         canvas.delete(target)
         canvas.delete(ball_of_gun)
-    if exsist_list(ball_of_gun) and canvas.coords(ball_of_gun)[2] > WIDTH_OF_CANVAS:
+    if exsist_ball(ball_of_gun) and canvas.coords(ball_of_gun)[2] > WIDTH_OF_CANVAS:
         print('Ядро улетело!')
         canvas.delete(ball_of_gun)
-    if exsist_list(target) and canvas.coords(target)[3] < 0:
+    if exsist_ball(target) and canvas.coords(target)[3] < 0:
         print('Цель улетела!')
         canvas.delete(target)
     root_window.after(DELAY, motion)
 
 
-def create_ball():
-    global vx1, vy1
-    vx1 = 0
-    vy1 = 0
+def create_ball(x, y, r):
+    global FIRE
+    FIRE = False
     print('Новое ядро')
-    return canvas.create_oval(0, HEIGHT_OF_CANVAS / 2, R, HEIGHT_OF_CANVAS / 2 + R, outline='red', fill='red')
+    return canvas.create_oval(x, y, r, y + r, outline='red', fill='red')
 
-def create_target():
-    global vx2, vy2
-    vx2 = 0
-    vy2 = -1
+
+def create_target(x, y, r):
     print('Новая цель')
-    return canvas.create_oval(WIDTH_OF_CANVAS - R2, HEIGHT_OF_CANVAS, WIDTH_OF_CANVAS, HEIGHT_OF_CANVAS - R2, outline='blue', fill='blue', tag='ball2')
+    return canvas.create_oval(x, y, x + r, y - r, outline='blue', fill='blue', tag='ball2')
 
 
 root_window = tkinter.Tk()
@@ -68,11 +78,12 @@ canvas.pack()
 canvas.focus_set()
 canvas.bind('<Up>', lambda event: canvas.move(ball_of_gun, 0, -10))
 canvas.bind('<Down>', lambda event: canvas.move(ball_of_gun, 0, 10))
-canvas.bind('<Home>', lambda event: canvas.coords(ball_of_gun, 0, HEIGHT_OF_CANVAS / 2, R, HEIGHT_OF_CANVAS / 2 + R))
+canvas.bind('<Home>', lambda event: canvas.coords(ball_of_gun, 0, HEIGHT_OF_CANVAS / 2, R1, HEIGHT_OF_CANVAS / 2 + R1))
 canvas.bind('<space>', lambda event: to_fire())
 
-ball_of_gun = create_ball()
-target = create_target()
+ball_of_gun = create_ball(X1, Y1, R1)
+
+target = create_target(X2, Y2, R2)
 
 motion()
 root_window.mainloop()
